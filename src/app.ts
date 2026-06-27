6df881f17d0f60065be39a47839b02b1da723c9a
aashraful349 <aashraful349@gmail.com>
2026-06-27 13:13:24 +0600

    made some minor changes

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,14 @@ import express, {
 import globalErrorHandler from "./middleware/globalErrorHandler";
 import { authRoute } from "./modules/auth/auth.route";
 import { issueRoute } from "./modules/issue/issue.route";
+import cors from "cors";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.text());
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Dev-Pulse");
